a177a73339bc5a70a414f56493bc3ea33bdfc9ac
MRizkiii <rizkigaming048@gmail.com>
2026-01-10 23:50:41 +0700

    add top attacker IPs, top attacked User

--- tesMain.py
+++ tesMain.py
@@ -5,8 +5,8 @@ def log(file):
 def is_valid_login_line(line):
     return "for " in line and "from " in line and "port " in line
 
-def if_True(line):
-    return "Accepted" in line or "Failed" in line or "invalid" in line
+# def if_True(line):
+#     return "Accepted" in line or "Failed" in line or "invalid" in line
 
 
 def parsing(x):
@@ -21,9 +21,9 @@ def parsing(x):
     Aport= x.find("port ", Bip) + 5
     Bport = x.find(" ", Aport)
 
-    # if x[Auser:Buser] == "invalid":
-    #     Aip = x.find("from ", Buser) + 5
-    #     Bip = x.find(" ", Aip)
+    if x[Auser:Buser] == "invalid":
+        Auser = x.find("invalid user ") + 13
+        Buser = x.find(" ", Auser)
 
     bulan = x.split()[0]
     tanggal = x.split()[1]
@@ -45,8 +45,13 @@ def parsing(x):
         else:
             status = "Failed Password"
 
+    elif "Starting session:" in x:
+        status = "Session Started (Shell)"
     elif "Invalid user" in x:
         status = "Invalid User"
+
+    elif "error" in x:
+        status = "Error"
 
 
     return {
@@ -59,30 +64,72 @@ def parsing(x):
         "status" : status
     }
 
+# def FailedLoginIPCount(line):
+
+
 with open("auth.log", "r") as f:
     logs = f.readlines()
     sshLog = log(logs)
-
-# for x in sshLog:
-#     if "invalid"in x:
-#         print(x)
 
 fullData = []
 
 for line in sshLog:
     if not is_valid_login_line(line):
         continue
-    if not if_True(line):
-        continue
+    # if not if_True(line):
+    #     continue
     data = parsing(line)
     fullData.append(data)
 
-# for x in fullData:
-    # if x["status"] == "unknown":
-    #     print(x)
-    # print(x)
-# print(fullData)
+
 for x in fullData:
-    if x["status"] == "unknown":
+    if x["status"] ==  "unknown":
         print(x)
 
+acceptedLog = []
+failedLog = []
+invalidUserLog = []
+
+
+
+for x in fullData:
+    if x["status"] == "Accepted password":
+        acceptedLog.append(x)
+    if x["status"] == "Failed Password":
+        failedLog.append(x)
+    if x["status"] == "Invalid User":
+        invalidUserLog.append(x)
+
+CounterIpFailed = []
+counterIP = {}
+
+for x in failedLog:
+    CounterIpFailed.append(x["ip"])    
+for x in CounterIpFailed:
+    counterIP[x] = counterIP.get(x, 0)+1
+sorted_ip = sorted(counterIP.items(), key=lambda item: item[1], reverse=True)
+
+print("=== TOP ATTACKER IPs ===")
+for ip, count in sorted_ip:
+    print(f"IP: {ip} | Total Serangan: {count}")
+
+allInvalidUser = []
+countInvUser = {}
+for x in invalidUserLog:
+    allInvalidUser.append(x["user"])
+for x in allInvalidUser:
+    countInvUser[x] = countInvUser.get(x, 0) +1
+sorted_user = sorted(countInvUser.items(), key=lambda item: item[1], reverse=True)
+
+
+print("=== TOP ATTACKED USER ===")
+for user, count in sorted_user: 
+    print(f"User: {user} | Total Serangan: {count}")
+
+
+
+
+
+# print(len(acceptedLog))
+# print(len(failedLog))
+# print(len(invalidUserLog))
